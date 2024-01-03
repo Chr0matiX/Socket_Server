3e9f7a8e88c7e69dcf70b31cf90a00e07c58fcf2
caochiyi <c18872123146@163.com>
2024-01-03 15:42:32 +0800

    新增“按逗号分隔字符串”功能

--- src/Socket/Util.hpp
+++ src/Socket/Util.hpp
@@ -26,8 +26,40 @@ namespace Socket_Util {
     }
 
     // 分割逗号字符串
-    static char** splStrByCom(char*) {
-
+    char** splitStr(const char* arg){
+        // 指针判空
+        if (arg == nullptr) {
+            return nullptr;
+        }
+        unsigned long long int cstrlen = strlen(arg);
+        // 计算有多少个子串
+        int count = 0;
+        for (int i = 0; i < cstrlen; i++) {
+            if (arg[i] == ',') { // 出现逗号
+                count++;
+            }
+        }
+        count++;
+        char** args = new char*[count + 1];
+        int argsIndex = 0;
+        char buffer[1024];
+        int buffIndex = 0;
+        for (int i = 0; i < cstrlen + 1; i++) {
+            if (arg[i] == ',' || arg[i] == '\0') { // 逗号结尾或文末的情况
+                buffer[buffIndex] = '\0';
+                char* tempStr = new char[strlen(buffer) + 1]; // 先开辟内存
+                strcpy(tempStr, buffer); // 拷贝到开辟的内存中
+                args[argsIndex] = tempStr; // 将开辟的内存存入char**
+                argsIndex++;
+                buffIndex = 0;
+                continue;
+            }
+            buffer[buffIndex] = arg[i];
+            buffIndex++;
+        }
+        args[argsIndex] = new char[1]; // 再加一个结尾符
+        args[argsIndex][0] = '\0';
+        return args;
     }
 }
 #endif // SOCKET_UTIL_HPP
